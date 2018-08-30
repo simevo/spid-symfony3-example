0e2a2f46e788e07c9035481912ede48928176228
manzolo <manzolo@libero.it>
2018-08-30 15:12:48 +0200

    fix get users info from spid

--- src/AppBundle/Security/SpidUser.php
+++ src/AppBundle/Security/SpidUser.php
@@ -7,28 +7,37 @@ use Symfony\Component\Security\Core\User\UserInterface;
 // https://api.symfony.com/3.4/Symfony/Component/Security/Core/User/UserInterface.html
 class SpidUser implements UserInterface
 {
+    private $username;
+    private $password;
+    private $salt;
+    private $roles;
+
     public function __construct($username, $password, $salt, array $roles)
     {
+        $this->username = $username;
+        $this->password = $password;
+        $this->salt = $salt;
+        $this->roles = $roles;
     }
 
     public function getRoles()
     {
-        return ['role'];
+        return $this->roles;
     }
 
     public function getPassword()
     {
-        return '';
+        return $this->password;
     }
 
     public function getSalt()
     {
-        return '';
+        return $this->salt;
     }
 
     public function getUsername()
     {
-        return 'user';
+        return $this->username;
     }
 
     public function eraseCredentials()
