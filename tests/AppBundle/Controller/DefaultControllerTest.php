4c92fe89c8362865cd492b6d24eee8df615217f3
d59495 <andrea.manzi@comune.fi.it>
2018-09-04 11:18:55 +0200

    Intro acs routing, travis ci integration, add tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+
     public function testIndex()
     {
         $client = static::createClient();
@@ -13,6 +14,68 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testReservedGuest()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/reserved');
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+        $this->assertContains('Authentication Required', $crawler->filter('body')->text());
+    }
+
+    public function testFullAccess()
+    {
+        $client = new \Goutte\Client();
+        // Do more configuration for the Goutte client
+        $driver = new \Behat\Mink\Driver\GoutteDriver($client);
+        $session = new \Behat\Mink\Session($driver);
+        // start the session
+        $session->start();
+
+        $session->visit('http://localhost:8000/reserved');
+        $page = $session->getPage();
+        $noauthcheck = (strpos($page->getHtml(), "Authentication Required") > 0);
+        $this->assertTrue($noauthcheck);
+
+        $session->visit('http://localhost:8000/login/testenv2');
+        $page->fillField('username', 'test');
+        $page->fillField('password', 'test');
+        $page->pressButton('Invia');
+        $page->pressButton('Invia');
+        $page->pressButton('Continua');
+        $fiscalnumbercheck = (strpos($page->getHtml(), "fiscalNumber") > 0);
+        $this->assertTrue($fiscalnumbercheck);
+
+        $session->visit('http://localhost:8000/reserved');
+        $page = $session->getPage();
+        $stuffcheck = (strpos($page->getHtml(), "Really reserved stuff here") > 0);
+        $this->assertTrue($stuffcheck);
+
+        /* FIXME: waiting for https://github.com/italia/spid-symfony-bundle/pull/9 */
+        $session->visit('http://localhost:8000/acs');
+        $page = $session->getPage();
+        /*
+          $stuffcheck = (strpos($page->getHtml(), "Really reserved stuff here") > 0);
+          $this->assertTrue($stuffcheck);
+
+         */
+    }
+
+    public function testMetadata()
+    {
+        $client = new \Goutte\Client();
+        // Do more configuration for the Goutte client
+        $driver = new \Behat\Mink\Driver\GoutteDriver($client);
+        $session = new \Behat\Mink\Session($driver);
+        // start the session
+        $session->start();
+        $session->visit('http://localhost:8000/metadata');
+        $page = $session->getPage();
+        $samlcheck = (strpos($page->getContent(), "urn:oasis:names:tc:SAML:2.0:metadata") > 0);
+        $this->assertTrue($samlcheck);
+    }
+
 }
